19071afec5992f524894df3bd636648d8e17aa28
TrongNhanDo <dotrongnhan15102000@gmail.com>
2022-05-18 21:30:21 +0700

    show thong bao

--- resources/views/pusher.blade.php
+++ resources/views/pusher.blade.php
@@ -7,6 +7,9 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <style type="text/css" media="screen">
+      body{
+        width: 100%;
+      }
       #messages{
         color: #1abc9c;
       }
@@ -14,6 +17,17 @@
         max-width: 50%;
         margin-bottom:10px;
         border-color: #34495e;
+      }
+      .tb{
+        display: none;
+        position: fixed;
+        top: 30px;
+        right: 30px;
+        height: 100px;
+        width: 100px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 3px;
       }
     </style>
 </head>
@@ -28,6 +42,9 @@
         <p>Message preview:</p>
           <ul id="messages" class="list-group"></ul>
       </div>
+    </div>
+    <div class="tb">
+      CÓ THÔNG BÁO MỚI
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -49,9 +66,14 @@
 
       //function add message
       function addMessageDemo(data) {
-        var liTag = $("<li class='list-group-item'></li>");
-        liTag.html(data.msg);
-        $('#messages').append(liTag);
+        // var liTag = $("<li class='list-group-item'></li>");
+        // liTag.html(data.msg);
+        // $('#messages').append(liTag);
+        $(".tb").show();
+        setTimeout(() => {
+          $(".tb").hide();
+          $("#messages").append('<h1>hahaah</h1>');
+        }, 3000);
       }
     </script>
 </body>
